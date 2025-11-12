feaaad0b8a1501baeb8a5cb3912de74884483c2e
reizucodes <blaisebueno@gmail.com>
2025-11-12 18:09:24 +0800

    feat: add description in parameter field

--- src/components/endpoint-form/ParameterField.tsx
+++ src/components/endpoint-form/ParameterField.tsx
@@ -10,12 +10,15 @@ interface Props {
 export function ParameterField({ parameter, allowed, onChange, onRemove }: Props) {
     return (
         <div className="p-2 border rounded grid grid-cols-6 gap-2 items-center">
+            {/* Name */}
             <input
                 className="col-span-2 p-1 border rounded"
                 value={parameter.name}
                 onChange={e => onChange({ ...parameter, name: e.target.value })}
-                placeholder='query'
+                placeholder="query"
             />
+
+            {/* Parameter Location */}
             <select
                 className="p-1 border rounded"
                 value={parameter.in}
@@ -25,6 +28,8 @@ export function ParameterField({ parameter, allowed, onChange, onRemove }: Props
                 {allowed.query && <option value="query">query</option>}
                 <option value="header">header</option>
             </select>
+
+            {/* Type */}
             <select
                 className="p-1 border rounded"
                 value={parameter.schemaType || 'string'}
@@ -35,17 +40,47 @@ export function ParameterField({ parameter, allowed, onChange, onRemove }: Props
                 <option value="boolean">boolean</option>
                 <option value="number">number</option>
             </select>
-            <label className="flex items-center gap-2">
-                <input
-                    type="checkbox"
-                    checked={!!parameter.required}
-                    onChange={e => onChange({ ...parameter, required: e.target.checked })}
-                />
-                required
-            </label>
-            <button className="text-sm text-red-600 hover:cursor-pointer justify-self-end" onClick={onRemove}>
-                <svg height= "22" width="22" fill="#ef4444" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z"></path></g></svg>
+
+            {/* Required Icon */}
+            <div
+                className="flex justify-center cursor-pointer"
+                title={parameter.required ? 'Required' : 'Required'}
+                onClick={() => onChange({ ...parameter, required: !parameter.required })}
+            >
+                {parameter.required ? (
+                    <svg className="w-5 h-5 text-red-600" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 1a9 9 0 1 0 9 9A9 9 0 0 0 10 1zm1 13H9v-2h2zm0-4H9V5h2z" />
+                    </svg>
+                ) : (
+                    <svg className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 1a9 9 0 1 0 9 9A9 9 0 0 0 10 1zm1 13H9v-2h2zm0-4H9V5h2z" />
+                    </svg>
+                )}
+            </div>
+
+            {/* Remove Button */}
+            <button
+                className="text-sm text-red-600 hover:cursor-pointer justify-self-end"
+                onClick={onRemove}
+            >   
+                <svg
+                    height="22"
+                    width="22"
+                    fill="#ef4444"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z" />
+                </svg>
             </button>
+
+            {/* Description Field */}
+            <input
+                className="col-span-6 p-1 border rounded mt-1"
+                value={parameter.description || ''}
+                onChange={e => onChange({ ...parameter, description: e.target.value })}
+                placeholder="description"
+            />
         </div>
     )
 }
